a337ac6e4e84fa548485c35b4aa3cf769e442c09
hikachuuuxd <hikarimizu1111@gmail.com>
2023-05-04 12:00:01 +0700

    update

--- dinamis-input-manipulation/script.js
+++ dinamis-input-manipulation/script.js
@@ -1,60 +1,43 @@
 
 const button = document.getElementById('target-button')
-const target = document.querySelector('#target')
-const text = document.querySelector('#target p')
-console.log(target)
-let select = document.getElementsByTagName('select')[0]
+const target = document.getElementById('target')
+const text = document.getElementById('target-text')
+let select  = document.getElementsByTagName('select')['target-select']
 let input = document.getElementById('target-input')
 
-select.addEventListener('input', function()
+button.addEventListener('click', function()
 {
+  
     if(select.value == 'bg')
     {
-        button.addEventListener('click', function()
-        {
             target.style.background = input.value
-        })
-
+            // console.log(select.value)
     }
     else if(select.value == 'font-color')
     {
-        button.addEventListener('click', function()
-        {
             text.style.color = input.value
-        })
     }
     else if(select.value == 'font-size')
     {
-        button.addEventListener('click', function()
-        {
-             target.style.fontSize = input.value
-        })
+             target.style.fontSize = input.value +'px'
     }
     else if(select.value == 'font')
     {
-        button.addEventListener('click', function()
-        {
              target.style.fontFamily = input.value
-        })
     }
     else if(select.value == 'width')
     {
-        button.addEventListener('click', function()
-        {
-             target.style.width = input.value
-        })
+           target.style.width = input.value + 'px'
     }
     else if(select.value == 'height')
     {
-        button.addEventListener('click', function()
-        {
-             target.style.height = input.value
-        })
+          target.style.height = input.value +'px' 
     }
     else
     {
         console.log('anda belum memasukkan pilihan')
     }
+
 
 })
 
