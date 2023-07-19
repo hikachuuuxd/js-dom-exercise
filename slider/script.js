f46b94b01a7c894e7812d800f41286706eb76416
hikachuuuxd <hikarimizu1111@gmail.com>
2023-07-19 16:39:55 +0700

    auto play gagal

--- slider/script.js
+++ slider/script.js
@@ -1,5 +1,27 @@
 const slider = document.querySelector('.slider');
-let isSlide = false, startX, startScrollLeft;
+const container = document.querySelector('.slider-container')
+const arrows = document.querySelectorAll('.slider-container a');
+const firstCard = document.querySelector('.card').offsetWidth;
+const sliderChild = [...slider.children];
+
+let cardPreview = Math.round(slider.offsetWidth / firstCard);
+
+sliderChild.slice(-cardPreview).reverse().forEach(card => {
+    slider.insertAdjacentHTML('afterbegin', card.outerHTML)
+})
+
+sliderChild.slice(0, cardPreview).forEach(card => {
+    slider.insertAdjacentHTML('beforeend', card.outerHTML)
+})
+arrows.forEach( arrow => {
+    arrow.addEventListener('click', function(){
+        slider.scrollLeft += arrow.id === 'left' ? -firstCard : firstCard
+    })
+})
+
+let isSlide = false, startX, startScrollLeft, timeoutId;
+
+
 const slideStart = (e) => {
     isSlide = true;
     slider.classList.add('slide')
@@ -15,7 +37,33 @@ const slide = (e) => {
     if(!isSlide) return;
     slider.scrollLeft = startScrollLeft - (e.pageX - startX)
 }
+
+const autoPlay = () => {
+   
+    timeoutId = setTimeout(() => slider.scrollLeft += -firstCard, 2500)
+}
+autoPlay();
+
+const infiniteScroll = () =>{
+    if(slider.scrollLeft === 0){
+        slider.classList.add('no-animation')
+        slider.scrollLeft = slider.scrollWidth - (2 * slider.offsetWidth )
+        slider.classList.remove('no-animation')
+    }else if(Math.ceil(slider.scrollLeft) == slider.scrollWidth - slider.offsetWidth){
+        slider.classList.add('no-animation')
+        slider.scrollLeft = slider.offsetWidth
+        slider.classList.remove('no-animation')
+    }
+
+    clearTimeout(timeoutId);
+    if(!container.matches(':hover')) autoPlay();
+}
+
+
+
 slider.addEventListener('mousemove', slide);
 slider.addEventListener('mousedown', slideStart);
 document.addEventListener('mouseup', slideStop);
-
+slider.addEventListener('scroll', infiniteScroll)
+container.addEventListener('mouseenter', () => clearTimeout(timeoutId))
+container.addEventListener('mouseleave', autoPlay)
